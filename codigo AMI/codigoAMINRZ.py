550ab8c142fa02c1fea76d1caf821a63c186769f
MartínTrujillo Bustamante <martin.trujillo1105@gmail.com>
2020-09-22 11:07:14 -0500

    update ami

--- codigo AMI/codigoAMINRZ.py
+++ codigo AMI/codigoAMINRZ.py
@@ -7,33 +7,42 @@ import numpy as np#Librerìa que agrega soporte para vectores y matrices
 Vb=input("Ingrese la velocidad binaria(bps): " );
 A=input("Ingrese la amplitud de la señal(v): ");
 tb=1.0/int(Vb);#Tiempo de bit
+tau=tb;#Ancho de pulso tau
+BWs=1/tau;#Ancho de banda
 paso=tb/100;#Tiempo de paso
 t=np.arange(0,tb,paso);#Vector de tiempo hasta 1 tiempo de bit
 s1=np.ones(len(t),dtype=int);#Dibujamos un vector de unos de tamaño un tiempo de bit
 s0=np.zeros(len(t));#Dibujamos un vector de ceros de tamaño 1 tiempo de bit
-#Secuencia aleatoria de bits
 n=10;
 x=np.random.randint(2,size=n);#Generamos una secuencia de bits aleatoria de tamaño n
+print("SOL:");
+print("Secuencia binaria: " + str(x));
 secuencia = [];#vector donde se almacena la secuencia de bits
 for i in range(n):#Dibujamos la secuncia de bits
     if x[i]==1:
-        secuencia=np.append(secuencia,s1);#Dibujamos un 1
+        secuencia=np.append(secuencia,s1*int(A));#Dibujamos un 1
     else:
         secuencia=np.append(secuencia,s0);#Dibujamos un 0
 
 t1=(len(secuencia))*paso;#Tiempo final donde termina la secuecia de bits
 tt=np.arange(0,t1,paso);#Vector de tiempo
-plt.figure(1);#Crear una nueva figura
+plt.figure(figsize=(7,8));#Crear una nueva figura
 plt.subplot(2,1,1)#Generamos el espacio para la imagen superior de la primera figura
 plt.plot(tt,secuencia,color='blue');#Graficamos la secuencia de bits
-plt.axis([0,t1,-0.5,1.5]);#Colocamos los limites de nuestra grafica
+plt.axis([0,t1,-0.2*int(A),1.2*int(A)]);#Colocamos los limites de nuestra grafica
 plt.grid(linestyle='-')#Colocamos un grid
 plt.title('Codigo UNIPOLAR NRZ');#Titulo de la grafica
 #plt.xlabel('Tiempo en (s)');#Etiqueta en el eje x
 plt.ylabel("Amplitud en voltios");#Etiqueta en el eje y
+#print("Código NRZ");
+#print("Tb: %E" % tb);
+#print("tau: %E" % tau);
+#print("BWs: %E" % BWs);
 
 #Codigo de linea AMI NRZ
 tbm=tb;#Tiempo de bit para AMI NRZ igual al tiempo de bit de la señal inicial
+taum=tb;#Ancho de pulso tau
+BWsm=1/tau;#Ancho de banda
 tm=np.arange(0,tbm,paso);#Vector de tiempo hasta 1 tiempo de bit
 secuenciaM=[];#Vector donde se almacenara la secuencia de bits AMI NRZ
 j=0;
@@ -41,9 +50,9 @@ for i in range(n):#Dibujamos la secuencia de bits
     if x[i]==1:
         j=j+1;
         if j%2==0:#Si es par pone -1
-            s1M=-np.ones(len(tm));
+            s1M=-int(A)*np.ones(len(tm));
         else:#Si es impar pone un 1
-            s1M=np.ones(len(tm));
+            s1M=int(A)*np.ones(len(tm));
         secuenciaM=np.append(secuenciaM,s1M);
     else:
         s0M=np.zeros(len(tm));
@@ -52,11 +61,14 @@ t2=(len(secuenciaM))*paso;#Tiempo final donde termina la secuencia de bits
 tt2=np.arange(0,t2,paso);#Vector de tiempo señal AMI NRZ
 plt.subplot(2,1,2);#Generamos el espacio para almacenar la imagen ingerior
 plt.plot(tt2,secuenciaM,color='red');#Graficamos la secuencia de bits
-plt.axis([0,t1,-1.5,1.5]);#Colocamos los limites de nuestra grafica
+plt.axis([0,t1,-1.2*int(A),1.2*int(A)]);#Colocamos los limites de nuestra grafica
 plt.grid(True)
 plt.title('Codigo AMI NRZ');#Titulo de la grafica
 #plt.xlabel('Tiempo en (s)');#Etiqueta en el eje x
 plt.ylabel("Amplitud en voltios")#Etiqueta en el eje y
+print("Tb: %E" % tbm);
+print("tau: %E" % taum);
+print("BWs: %E" % BWsm);
 
 #Densidad espectral de potencia
 plt.figure(2);#Creamos una nueva figura
@@ -66,6 +78,14 @@ S=fourier.fft(S);
 S=fourier.fftshift(S);
 fo=1/(N*paso);#Frecuencia de paso
 F=np.arange(-N/2,N/2,1);#Vector de frecuencias
+#plt.subplot(211);
 plt.plot(F,np.abs(S));#Graficamos la densidad espectral de potencia
+plt.axis([-50,50,0,np.amax(abs(S))]);
+#plt.subplot(212);
+#plt.psd(secuenciaM,1024, 1/0.01);
+#plt.xlabel('Frequencia')
+#plt.ylabel('PSD(db)')
+#plt.axis([0,10,0,3]);
+input("Press Enter to continue...");
 plt.show()
 
--- codigo AMI/codigoAMIRZ.py
+++ codigo AMI/codigoAMIRZ.py
@@ -4,33 +4,42 @@ import scipy.fftpack as fourier
 import numpy as np
 
 #Ingresamos parametros para la señal
-tb=1;
-paso=1/100;
+Vb=input("Ingrese la velocidad binaria(bps): ");
+A=input("Ingrese la amplitud de la señal(v): ");
+tb=1/int(Vb);#Tiempo de bit
+tau=tb;#Ancho de pulso tau
+BWs=1/tau;#Ancho de banda
+paso=tb/100;#Tiempo de paso
 t=np.arange(0,tb,paso);
 s1=np.ones(len(t),dtype=int);
 s0=np.zeros(len(t));
 #Secuencia aleatoria de bits
 n=10;
 x=np.random.randint(2,size=n);
+print("SOL:");
+print("Secuencia binaria: " + str(x));
 secuencia = [];
 for i in range(n):
     if x[i]==1:
-        secuencia=np.append(secuencia,s1);
+        secuencia=np.append(secuencia,s1*int(A));
     else:
         secuencia=np.append(secuencia,s0);
 
 t1=(len(secuencia))*paso;
 tt=np.arange(0,t1,paso);
-plt.figure(1);
+plt.figure(figsize=(7,8));
 plt.subplot(2,1,1)
 plt.plot(tt,secuencia,color='blue')
-plt.axis([0,t1,-0.5,1.5]);
+plt.axis([0,t1,-0.2*int(A),1.2*int(A)]);
 plt.grid(linestyle='-')
 plt.title('Codigo UNIPOLAR NRZ');
 #plt.xlabel('Tiempo en (s)');
 plt.ylabel("Amplitud en voltios")
 
 #Codigo de linea AMI RZ
+tbami=tb;#Tiempo de bit para AMI NRZ igual al tiempo de bit de la señal inicial
+tauami=tb/2;#Ancho de pulso tau
+BWsami=1/tauami;#Ancho de banda
 tbm=tb/2;
 tm=np.arange(0,tbm,paso);
 secuenciaM=[];
@@ -39,10 +48,10 @@ for i in range(n):
     if x[i]==1:
         j=j+1;
         if j%2==0:
-            s1M=-np.ones(len(tm));
+            s1M=-int(A)*np.ones(len(tm));
             s1M=np.append(s1M,np.zeros(len(tm)));
         else:
-            s1M=np.ones(len(tm));
+            s1M=int(A)*np.ones(len(tm));
             s1M=np.append(s1M, np.zeros(len(tm)));
         secuenciaM=np.append(secuenciaM,s1M);
     else:
@@ -53,11 +62,14 @@ t2=(len(secuenciaM))*paso;
 tt2=np.arange(0,t2,paso);
 plt.subplot(2,1,2);
 plt.plot(tt2,secuenciaM,color='red')
-plt.axis([0,t1,-1.5,1.5]);
+plt.axis([0,t1,-1.5*int(A),1.5*int(A)]);
 plt.grid(True)
 plt.title('Codigo AMI RZ');
 #plt.xlabel('Tiempo en (s)');
 plt.ylabel("Amplitud en voltios")
+print("Tb: %E" % tbami);
+print("tau: %E" % tauami);
+print("BWs: %E" % BWsami);
 
 #Densidad espectral de potencia
 plt.figure(2);
@@ -68,5 +80,7 @@ S=fourier.fftshift(S);
 fo=1/(N*paso);
 F=np.arange(-N/2,N/2,1);
 plt.plot(F,np.abs(S));
+plt.axis([-50,50,0,np.amax(abs(S))]);
+input("Press Enter to continue...");
 plt.show()
 
